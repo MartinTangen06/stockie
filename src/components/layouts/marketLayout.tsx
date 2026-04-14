7f56ed745b67f644ebdde28a58fd80f65a1d8306
Martin tangen <martin.tangen@tangenlab.no>
2026-04-14 07:44:39 +0200

    renamed app

--- src/components/layouts/marketLayout.tsx
+++ src/components/layouts/marketLayout.tsx
@@ -5,12 +5,7 @@ export const MarketLayout = ({ symbol }: { symbol: string }) => {
 
     return (
         <div>
-            <h1>Market Status for {symbol}</h1>
-            <p>Current Price: {marketStatus['c']}</p>
-            <p>High Price of the day: {marketStatus['h']}</p>
-            <p>Low Price of the day: {marketStatus['l']}</p>
-            <p>Open Price of the day: {marketStatus['o']}</p>
-            <p>Previous Close Price: {marketStatus['pc']}</p>
+            {marketStatus}
         </div>
     );
 }
---
+++ src/dtos/stock.ts
@@ -0,0 +1,9 @@
+export default interface Stock {
+    c: number; // Current price
+    dp: number; // Percentage change
+    d: number; // Absolute change
+    h: number; // High price of the day
+    l: number; // Low price of the day
+    o: number; // Open price of the day
+    pc: number; // Previous close price
+}
